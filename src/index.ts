2c20963b867c1e4eb450e55de39adb53dbbd8caf
Yassata <taha@mocka.app>
2023-11-22 22:48:56 +0100

    ls

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  return response.status(200).json({ hello: 'world' });
+}
